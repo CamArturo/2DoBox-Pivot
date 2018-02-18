ad8af52e34b26409263f8f60536a4a7f19240e14
Camilo Snapp <camilo.snapp@gmail.com>
2018-02-17 22:43:15 -0700

    Renamed HTML to todo, updated folder names, added complete button, however, not working yet.

--- js/script.js
+++ js/script.js
@@ -1,52 +1,57 @@
-
-
-$('.form__input-body').on('keyup', toggleDisableState);
-$('.form__input-title').on('keyup', toggleDisableState);
-$('.section__ideas').on('click', '.delete-x', deleteIdeas);
-$('.section__ideas').on('click', '.upvote', upvoteIdea);
-$('.section__ideas').on('click', '.downvote', downvoteIdea);
+$('.todo-form__input-title').on('keyup', toggleDisableState);
+$('.todo-form__input-body').on('keyup', toggleDisableState);
+$('.todo-list__ideas').on('click', '.delete-x', deleteIdeas);
+$('.todo-list__ideas').on('click', '.upvote', upvoteIdea);
+$('.todo-list__ideas').on('click', '.downvote', downvoteIdea);
 // $sectionIdea.on('keydown', '.idea-title', persistTitle);
 // $sectionIdea.on('keydown', '.idea-body', persistBody);
-$('.section__search-field').on('keyup', searchIdeas);
-$('.form__button-save').on('click', saveIdea);
-$('.section__ideas').on('click','.checkMark',changeReadClass);
+$('.todo-list__search-field').on('keyup', searchIdeas);
+$('.todo-list__ideas').on('click', '.checkMark', changeReadClass);
+$('.todo-form__button-save').on('click', saveIdea);
+$('.completedBtn').on('click', saveIdea);
 
-
-function changeReadClass(ev){
+function changeReadClass(ev) {
   $(this).closest('.checkMark').toggleClass('checkMarkActive');
-  if('checkMarkActive'){
-  $(this).closest('.idea-cards').toggleClass('idea-cards-read');
-}
+  if ('checkMarkActive') {
+    $(this).closest('.idea-cards').toggleClass('idea-cards-read');
+  }
 }
 
 populatingIdeas();
 
-function Idea(id, ideaTitleValue, ideaBodyValue) {
-  this.id = id;
+function Idea(key, ideaTitleValue, ideaBodyValue) {
+  this.levelsImportance = [
+    'Critical',
+    'High',
+    'Normal',
+    'Low',
+    'None'
+  ];
+  this.key = key;
   this.title = ideaTitleValue;
   this.body = ideaBodyValue;
-  this.quality = 'quality: swill';
   this.completed = false;
+  this.importanceValue = 2;
 }
 
 function saveIdea(event) {
   event.preventDefault();
   var key = Date.now();
-  var $inputTitle = $('.form__input-title');
-  var $inputBody = $('.form__input-body');
+  var $inputTitle = $('.todo-form__input-title');
+  var $inputBody = $('.todo-form__input-body');
   var newIdea = new Idea(key, $inputTitle.val(), $inputBody.val());
   sendToStorage(newIdea);
-  $('.section__ideas').prepend(`
-  <article class="idea-cards" id="${newIdea.id}">
+  $('.todo-list__ideas').prepend(`
+  <article class="idea-cards" id="${newIdea.key}">
     <p class = "checkMark">&#x02713</p>
     <h2 class="idea-title" contenteditable="true">${newIdea.title}</h2>
     <article class="delete-x"></article>
     <p class="idea-body" contenteditable="true">${newIdea.body}</p>
     <article class="upvote"></article>
     <article class="downvote"></article>
-    <h3 class="quality">${newIdea.quality}</h3>
+    <button class="completedBtn">Completed Task</button>
   </article>`);
-  $('form').trigger("reset")
+  $('form').trigger("reset");
   $inputTitle.focus();
   toggleDisableState();
 }
@@ -55,26 +60,26 @@ function populatingIdeas() {
   for (var i = 0; i < localStorage.length; i++) {
     var stringifiedObject = localStorage.getItem(localStorage.key(i));
     var idea = JSON.parse(stringifiedObject);
-    $('.section__ideas').prepend(`<article class="idea-cards" id=${idea.id}>
+    $('.todo-list__ideas').prepend(`<article class="idea-cards" id=${idea.key}>
       <h2 class="idea-title" contenteditable="true">${idea.title}</h2>
       <article class="delete-x" aria-label="Button to delete idea"></article>
       <p class="idea-body" contenteditable="true">${idea.body}</p>
       <article class="upvote"></article>
       <article class="downvote"></article>
-      <h3 class="quality">${idea.quality}</h3>
+      <button class="completedBtn">Completed Task</button>
       </article>`);
   }
 }
 
 function sendToStorage(idea) {
   var stringifiedIdea = JSON.stringify(idea);
-  localStorage.setItem(idea.id, stringifiedIdea)
+  localStorage.setItem(idea.key, stringifiedIdea)
 }
 
 function toggleDisableState() {
-  var $inputTitle = $('.form__input-title');
-  var $inputBody = $('.form__input-body');
-  var $saveBtn = $('.form__button-save');
+  var $inputTitle = $('.todo-form__input-title');
+  var $inputBody = $('.todo-form__input-body');
+  var $saveBtn = $('.todo-form__button-save');
   if ($inputBody.val() && $inputTitle.val()) {
     $saveBtn.prop('disabled', false);
   } else {
@@ -84,17 +89,17 @@ function toggleDisableState() {
 
 function deleteIdeas() {
   $(this).closest('.idea-cards').fadeOut();
-  var id = $(this).closest('.idea-cards').attr('id');
-  localStorage.removeItem(id);
+  var key = $(this).closest('.idea-cards').attr('id');
+  localStorage.removeItem(key);
 }
 
 function changeStorageQuality(newthis) {
-  var id = $(newthis).closest('.idea-cards').attr('id');
-  var idea = localStorage.getItem(id);
+  var key = $(newthis).closest('.idea-cards').attr('id');
+  var idea = localStorage.getItem(key);
   idea = JSON.parse(idea);
   idea.quality = $(newthis).siblings('h3').text();
-  var stringifiedIdea = JSON.stringify(idea)
-  localStorage.setItem(id, stringifiedIdea);
+  var stringifiedIdea = JSON.stringify(idea);
+  localStorage.setItem(key, stringifiedIdea);
 }
 
 function upvoteIdea() {
@@ -116,7 +121,7 @@ function downvoteIdea() {
 }
 
 // function persistTitle(e) {
-//   var $inputTitle = $('.form__input-title');
+//   var $inputTitle = $('.todo-form__input-title');
 //   if (e.keyCode === 13) {
 //     e.preventDefault();
 //     $inputTitle.focus();
@@ -130,20 +135,19 @@ function downvoteIdea() {
 //   localStorage.setItem(id, stringifiedIdea);
 // }
 
-function searchIdeas(){
-  var $input = $('.section__search-field').val();
+function searchIdeas() {
+  var $input = $('.todo-list__search-field').val();
   $input = $input.toUpperCase();
   var card = $('.idea-cards');
-    for (i = 0; i < card.length; i++){
+  for (i = 0; i < card.length; i++) {
     var title = $(card[i]).find('.idea-title').text().toUpperCase();
     var body = $(card[i]).find('.idea-body').text().toUpperCase();
     var quality = $(card[i]).find('.quality').text().toUpperCase();
-    quality = quality.replace('QUALITY: ','');
-    if(title.includes($input)|| body.includes($input) || quality.includes($input)){
-    $(card[i]).show();
-    } else { 
-    $(card[i]).hide();
+    quality = quality.replace('QUALITY: ', '');
+    if (title.includes($input) || body.includes($input) || quality.includes($input)) {
+      $(card[i]).show();
+    } else {
+      $(card[i]).hide();
     }
   }
 }
-
